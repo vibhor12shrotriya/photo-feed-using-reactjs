1059cfe5b89094053bb15877e9b61ad80f6aed8c
Vibhor shrotriya <32879438+vibhor12shrotriya@users.noreply.github.com>
2020-11-13 12:10:24 +0530

    Add files via upload

---
+++ Heading.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  max-width: 70rem;
+  margin: 2rem auto;
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font-family: 'Oswald', sans-serif;
+  margin-bottom: 1em;
+`;
+
+
+
+export const Heading = () => {
+
+  return (
+    <Header>
+      <H1>Photo Feed</H1>
+      <p>Photo Feed Using ReactJs</p>
+      <p></p>
+    </Header>
+  )
+}
