899e5e3b2c2e89b72dfba877e0a93068bd251ee7
Vibhor shrotriya <32879438+vibhor12shrotriya@users.noreply.github.com>
2020-11-13 12:23:59 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './main.css'
+
+
+function Header(){
+    return(<div>
+        <header>
+            <img src='https://pngimg.com/uploads/trollface/trollface_PNG4.png' alt='trollface'/>
+            <p className='header-slogan'>Meme Generator</p>
+        </header>
+    </div>)
+}
+
+export default Header
